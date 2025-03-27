d030cd4cc825bb84d48e3317ebf467d515eb7afa
yurii-bel <dragonwow300@gmail.com>
2025-03-27 18:57:24 +0200

    feat: add middleware & clerk provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Roboto } from "next/font/google";
 import "./globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
 
 const roboto = Roboto({
   weight: "400",
@@ -18,8 +19,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={`${roboto.className}`}>{children}</body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body className={`${roboto.className}`}>{children}</body>
+      </html>
+    </ClerkProvider>
   );
 }
