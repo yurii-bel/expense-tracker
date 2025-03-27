2354b483ea8ca96423e8039aeb02157f82adbdca
yurii-bel <dragonwow300@gmail.com>
2025-03-27 19:12:13 +0200

    feat: add check user functionality

---
+++ components/Header.tsx
@@ -0,0 +1,21 @@
+import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
+
+const Header = () => {
+  return (
+    <nav className="navbar">
+      <div className="navbar-container">
+        <h2>Expense Tracker</h2>
+        <div>
+          <SignedOut>
+            <SignInButton />
+          </SignedOut>
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Header;
---
+++ lib/checkUser.ts
@@ -0,0 +1,36 @@
+import { currentUser } from "@clerk/nextjs/server";
+
+import { db } from "@/lib/db";
+
+export const checkUser = async () => {
+  const user = await currentUser();
+
+  // Check for current logged in clerk user
+  if (!user) {
+    return null;
+  }
+
+  // Check if user exists in database
+  const loggedInUser = await db.user.findUnique({
+    where: {
+      clerkUserId: user.id,
+    },
+  });
+
+  // If user is in database, return user
+  if (loggedInUser) {
+    return loggedInUser;
+  }
+
+  // If user is not in database, create user
+  const newUser = await db.user.create({
+    data: {
+      clerkUserId: user.id,
+      name: `${user.firstName} ${user.lastName}`,
+      imageUrl: user.imageUrl,
+      email: user.emailAddresses[0].emailAddress,
+    },
+  });
+
+  return newUser;
+};
