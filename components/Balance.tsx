ccc3e905bb90ac9f48c2d090c68f924db4aff8eb
yurii-bel <dragonwow300@gmail.com>
2025-03-28 20:53:30 +0200

    feat: add AddComas utility function

--- components/Balance.tsx
+++ components/Balance.tsx
@@ -1,11 +1,12 @@
 import getUserBalance from "@/app/actions/getUserBalance";
+import { AddCommas } from "@/lib/utils";
 
 const Balance = async () => {
   const { balance } = await getUserBalance();
   return (
     <>
       <h4>Your Balance</h4>
-      <h1>${balance}</h1>
+      <h1>$AddCommas(balance ?? 0)</h1>
     </>
   );
 };
