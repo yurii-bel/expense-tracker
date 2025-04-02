d22e0ff181164e05b75cb12144492ab2363b5486
yurii-bel <dragonwow300@gmail.com>
2025-04-02 21:32:39 +0300

    feat: add income-expense component

---
+++ components/IncomeExpense.tsx
@@ -0,0 +1,16 @@
+const IncomeExpense = () => {
+  return (
+    <div className="inc-exp-container">
+      <div>
+        <h4>Income</h4>
+        <p className="money plus">$700</p>
+      </div>
+      <div>
+        <h4>Expense</h4>
+        <p className="money minus">$200</p>
+      </div>
+    </div>
+  );
+};
+
+export default IncomeExpense;
